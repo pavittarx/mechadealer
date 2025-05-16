20e95aca4714f68748ce04cc40e1cf49e17c82be
pavittarx <29155477+pavittarx@users.noreply.github.com>
2025-05-16 01:54:08 +0000

    feat: updated tables schemas for storing orders and trades

--- libs/store/models.py
+++ libs/store/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -10,10 +11,10 @@ class User(BaseModel):
     capital: float = Field(default=0)
     capital_used: float = Field(default=0)
     capital_remaining: float = Field(default=0)
-    created_at: str = Field(default="now()")
-    updated_at: str = Field(default="now()")
     is_active: bool = Field(default=False)
     is_verified: bool = Field(default=False)
+    created_at: str = Field(default="now()")
+    updated_at: str = Field(default="now()")
 
 
 class UserTransactions(BaseModel):
@@ -33,6 +34,40 @@ class Strategies(BaseModel):
     capital_used: float
     capital_remaining: float
     leverage: float = Field(default=1)
+    pnl: float = Field(default=0)
     unrealized_pnl: float = Field(default=0)
     realized_pnl: float = Field(default=0)
     created_at: str = Field(default="now()")
+
+
+class Order(BaseModel):
+    id: int
+    strategy_id: int
+    broker_id: str
+    dt: str
+    ticker: str
+    quantity: float
+    exit_quantity: float = Field(default=0)
+    action: str
+    type: str
+    price: float
+    order_type: str
+    capital_used: float
+    margin_used: float
+    charges: float
+    ref_id: int
+    is_filled: bool = Field(default=False)
+    is_cancelled: bool = Field(default=False)
+    is_active: bool = Field(default=True)
+    version: int = Field(default=1)
+    created_at: str = Field(default="now()")
+    updated_at: str = Field(default="now()")
+
+
+class TradeStats(BaseModel):
+    id: int
+    strategy_id: int
+    strategy_name: str
+    order_id: int
+    ticker: str
+    pnl: float = Field(default=0)
