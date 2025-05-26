4672598e5a98b388173534d6b5c747159bf1a519
Pavittar Singh <29155477+pavittarx@users.noreply.github.com>
2025-05-26 09:25:54 +0530

    feat/webapp: frontend for the mechadealer project (#8)
    * [webapp]: initial commit
    
    * feat: webapp frontend v0

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// Pages will be automatically routed by Nuxt
+// when <NuxtPage /> is used.
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
